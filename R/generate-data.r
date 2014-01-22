6c9e533f0754b5b818f2a8dbfc75c2e33d850937
Joe Thorley <joe@poissonconsulting.ca>
2014-01-21 19:26:01 -0600

    added obs_by arg plus updated documentation

--- R/generate-data.r
+++ R/generate-data.r
@@ -6,26 +6,28 @@
 #' 
 #' @details
 #' Unless a variable is named in range it is fixed at its base value. 
-#' A logical variable base value is FALSE while a factor's is
-#' its first level. All other variables are set to be the mean value
+#' A continuous variable's base value is its mean value
 #' of the same class, i.e., an integer variable's base value is its rounded mean.
+#' A logical variable's base value is FALSE while a factor's is
+#' its first level. The only exception to this if a variable is named in obs_by 
+#' in which case its observed base is its closest observed value to its base value.
 #' 
-#' If a variable is specified in range then it is represented
-#' by a sequence of as evenly spaced as possible unique values of the same class 
+#' Alternatively if a variable is named in range then a sorted sequence of 
+#' unique values of the same class 
 #' from the minimum to the maximum of the observed values with a 
-#' length of length_out (by default 30). The only exceptions are if the number of
-#' unique values between the maximum or minimum are less than length_out or 
-#' logical variables
-#' for which the only values returned are FALSE and TRUE and factors for which all levels
-#' are returned.  
+#' length of up to length_out (by default 30) is generated. If length_out is less
+#' than the number of possible values then in the case of a continous variable the
+#' values are as evenly distributed as possible while in the case of a factor only
+#' the first length_out levels are selected. If a variable is also
+#' named in obs_by then only observed values are permitted in the sequence and
+#' continuous variables may no longer be as evenly spaced as possible.
 #' 
-#' The generated data frame consists of all combinations of the values for the
-#' variables in range together with the base value for the other variables.
-#' 
-#' @param data the data.frame of variables from which the data.frame will be generated
-#' @param range a character vector of the variables in data to 
-#' represent by a sequence of values
-#' @param length_out the the number of values in a sequence
+#' @param data data.frame of variables from which the data.frame will be generated
+#' @param range character vector of the variables in data to 
+#' represent by a sequence of values or NULL (the default)
+#' @param obs_by character vector of the variables in data to 
+#' represent by a sequence of observed values or NULL (the default)
+#' @param length_out integer scalar of the maximum number of values in a sequence
 #' @return The generated data.frame which can then be passed to a model for the
 #' purpose of estimating the effects of particular variables.
 #' @examples
@@ -34,29 +36,35 @@
 #'    posixt = ISOdate(2000,1,1) + 1:10)
 #'    
 #' generate_data (data)
-#' generate_data (data,range='numeric')
-#' generate_data (data,range=c('date','factor'))
+#' generate_data (data, obs_by = c("numeric"))
+#' generate_data (data, range = "numeric")
+#' generate_data (data, range = c("date", "factor"))
+#' generate_data (data, range = c("numeric"), obs_by = c("numeric"))
+#' 
 #' @export
-generate_data <- function (data, range = NULL, length_out = 30) {
+generate_data <- function (data, range = NULL, obs_by = NULL, length_out = 30) {
+  
   assert_that(is.data.frame(data))
   assert_that(is.null(range) || is.character(range))
+  assert_that(is.null(obs_by) || is.character(obs_by))
   assert_that(is.count(length_out) && noNA(length_out))
-
-  range <- as.character(range)
-  length_out <- as.integer(length_out)
   
-  x <- range[!range %in% c(colnames (data),"")]
+  x <- range[!range %in% colnames (data)]
   if (length(x))
-    message (paste(c("the following variables are in range but not data:", x), 
-                   collapse = " "))  
+    warning("the following variables are in range but not data: ", x)
+  
+  x <- obs_by[!obs_by %in% colnames (data)]
+  if (length(x))
+    warning("the following variables are in obs_by but not data: ", x)
   
   dat<-list()
   for (colname in colnames(data)) {
-    variable <- dvariable(data[, colname, drop=T])    
+    variable <- variable(data[, colname, drop = TRUE])
     if (colname %in% range) {
-        dat[[colname]] <- generate_range(variable, length_out=length_out)        
+        dat[[colname]] <- get_range(variable, observed = colname %in% obs_by,
+                                    length_out=length_out)
     } else
-      dat[[colname]] <- base(variable)
+      dat[[colname]] <- get_base(variable, observed = colname %in% obs_by)      
   }
   data <- expand.grid (dat)
   
