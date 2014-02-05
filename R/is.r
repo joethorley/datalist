a3f932d1b70ee45947f62de9c949e22f2c52ef8f
Joe Thorley <joe@poissonconsulting.ca>
2014-02-05 10:22:37 -0600

    added is_converted_data_frame and is_converted_data_list

--- R/is.r
+++ R/is.r
@@ -75,6 +75,49 @@ is_convertible_data <- function (x) {
 #' @seealso \code{\link{is_convertible_data}} and 
 #' \code{\link{convert_data}}
 #' @examples
+#' is_converted_data_list(trees)
+#' is_converted_data_list(convert_data(trees))
+#' is_converted_data_list(list(x = 1:10, y = 0.33, z = matrix(1:9, ncol = 3)))
+#' is_converted_data_list(list(x = 1:10, y = 0.33, z = matrix(1:9, ncol = 3)))
+#' is_converted_data_list(list(x = factor(1:10), y = 0.33, z = matrix(1:9, ncol = 3)))
+#' @export
+is_converted_data_list <- function (x) {
+  if(is.data.frame(x) || !is.list(x))
+    return (FALSE)
+  
+  all(sapply(x, is.numeric))
+}
+
+#' @title Test data set
+#'
+#' @description
+#' Tests whether x is converted data that can be input into
+#' JAGS, WinBUGS or OpenBUGS.
+#' @param x the object to test
+#' @return A flag (logical scalar) indicating whether converted data
+#' @seealso \code{\link{is_convertible_data}} and 
+#' \code{\link{convert_data}}
+#' @examples
+#' is_converted_data_frame(trees)
+#' is_converted_data_frame(convert_data(trees))
+#' @export
+is_converted_data_frame <- function (x) {
+  if(!is.data.frame(x))
+    return (FALSE)
+  
+  all(sapply(x, is.numeric))
+}
+
+#' @title Test data set
+#'
+#' @description
+#' Tests whether x is converted data that can be input into
+#' JAGS, WinBUGS or OpenBUGS.
+#' @param x the object to test
+#' @return A flag (logical scalar) indicating whether converted data
+#' @seealso \code{\link{is_convertible_data}} and 
+#' \code{\link{convert_data}}
+#' @examples
 #' is_converted_data(trees)
 #' is_converted_data(convert_data(trees))
 #' is_converted_data(list(x = 1:10, y = 0.33, z = matrix(1:9, ncol = 3)))
@@ -82,7 +125,7 @@ is_convertible_data <- function (x) {
 #' is_converted_data(list(x = factor(1:10), y = 0.33, z = matrix(1:9, ncol = 3)))
 #' @export
 is_converted_data <- function (x) {
-  if(is.data.frame(x) || !is.list(x))
+  if(!is.list(x))
     return (FALSE)
   
   all(sapply(x, is.numeric))
