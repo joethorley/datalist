455758e08253f03e9c9205e8ae120237a9a48529
Joe Thorley <joe@poissonconsulting.ca>
2014-02-05 10:14:40 -0600

    simpler message

--- R/on-failure.r
+++ R/on-failure.r
@@ -1,16 +1,11 @@
 on_failure(is_convertible_data_list) <- function(call, env) {
-  paste0(deparse(call$x), " is not a list with logical, integer, 
-         or numeric vectors, matrices or arrays and factor, Date, 
-         and POSIXt vectors as the only variables")
+  paste0(deparse(call$x), " is not a convertible list")
 }
 
 on_failure(is_convertible_data_frame) <- function(call, env) {
-  paste0(deparse(call$x), " is not a data.frame with logical, integer, 
-         numeric, factor, Date, and POSIXt vectors as the only variables")
+  paste0(deparse(call$x), " is not a convertible data.frame")
 }
 
 on_failure(is_convertible_data) <- function(call, env) {
-  paste0(deparse(call$x), " is not a data.frame (or list) with logical, integer, 
-         or numeric vectors (or matrices or arrays) and factor, Date, 
-         and POSIXt vectors as the only variables")
+  paste0(deparse(call$x), " is not a convertible data.frame or list")
 }
